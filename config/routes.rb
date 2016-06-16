79718a1cda49bfeb13bc32d05242e2191600ac7b
Yatharth.Sharma <Yatharth@Yatharths-MacBook-Pro.local>
2016-06-17 03:19:39 +0530

    Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :products
+  root :to => 'products#index'
+  devise_scope :user do
+    get '/sign-in' => "devise/sessions#new", :as => :login
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
